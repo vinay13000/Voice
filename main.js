298b01ae16dae3cd67dea535aa89332e8e8053e3
vinay13000 <74045128+vinay13000@users.noreply.github.com>
2021-07-17 16:53:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+var SpeechRecognition=window.webkitSpeechRecognition;
+var recognition=new SpeechRecognition();
+var Textbox=document.getElementById("textbox")
+   function start(){
+      Textbox.innerHTML="";
+       recognition.start();
+
+   }   
+   recognition.onresult=function (event){
+       console.log(event);
+       var Content=event.results[0][0].transcript;
+       console.log(Content);
+       Textbox.innerHTML=Content;
+       console.log(Content);
+       if(Content=="Take my selfie"){
+         
+         console.log("Taking selfie ");
+         speak();
+
+       }
+       
+     }     
+   
+   function speak(){
+      var synth= window.speechSynthesis;
+      speak_data="Taking your selfie in 5 sec"
+      var utterThis= new SpeechSynthesisUtterance(speak_data);
+      synth.speak(utterThis);
+       Webcam.attach(camera);
+      
+       setTimeout(function (){
+         take_snapshot();
+         save();
+       },2000)
+
+
+   }
+   camera=document.getElementById("camera");
+   Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+  });
+  
+
+ function take_snapshot(){
+   Webcam.snap(function (data_uri){
+     document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>'
+   });
+   
+ }
+
+ function save() { link = document.getElementById("link");
+  image = document.getElementById("selfie_image").src ;
+   link.href = image; link.click(); }
